de1d7ed341d5a6017d2fca8a8ee555bb55110d8c
Luiz Felipe <luizfelipe0527@gmail.com>
2024-08-19 22:52:46 -0300

    feat: implement user login function

--- trip-planner-user-service/src/main/java/com/felipe/trip_planner_user_service/controllers/AuthController.java
+++ trip-planner-user-service/src/main/java/com/felipe/trip_planner_user_service/controllers/AuthController.java
@@ -1,17 +1,22 @@
 package com.felipe.trip_planner_user_service.controllers;
 
+import com.felipe.trip_planner_user_service.dtos.UserLoginDTO;
+import com.felipe.trip_planner_user_service.dtos.UserLoginResponseDTO;
 import com.felipe.trip_planner_user_service.dtos.UserRegisterDTO;
 import com.felipe.trip_planner_user_service.dtos.UserResponseDTO;
 import com.felipe.trip_planner_user_service.models.User;
 import com.felipe.trip_planner_user_service.services.UserService;
 import com.felipe.trip_planner_user_service.utils.response.CustomResponseBody;
 import com.felipe.trip_planner_user_service.utils.response.ResponseConditionStatus;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/auth")
@@ -25,7 +30,7 @@ public class AuthController {
 
   @PostMapping("/register")
   @ResponseStatus(HttpStatus.CREATED)
-  public CustomResponseBody<UserResponseDTO> register(@RequestBody UserRegisterDTO userRegisterDTO) {
+  public CustomResponseBody<UserResponseDTO> register(@RequestBody @Valid UserRegisterDTO userRegisterDTO) {
     User createdUser = this.userService.register(userRegisterDTO);
     UserResponseDTO userResponseDTO = new UserResponseDTO(createdUser);
 
@@ -34,6 +39,23 @@ public class AuthController {
     response.setCode(HttpStatus.CREATED);
     response.setMessage("Usuário criado com sucesso");
     response.setData(userResponseDTO);
+    return response;
+  }
+
+  @PostMapping("/login")
+  @ResponseStatus(HttpStatus.OK)
+  public CustomResponseBody<UserLoginResponseDTO> login(@RequestBody @Valid UserLoginDTO loginDTO) {
+    Map<String, Object> login = this.userService.login(loginDTO);
+    User user = (User) login.get("user");
+    String token = (String) login.get("token");
+    UserResponseDTO userInfo = new UserResponseDTO(user);
+    UserLoginResponseDTO userLoginResponseDTO = new UserLoginResponseDTO(userInfo, token);
+
+    CustomResponseBody<UserLoginResponseDTO> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.SUCCESS);
+    response.setCode(HttpStatus.OK);
+    response.setMessage("Usuário logado com sucesso");
+    response.setData(userLoginResponseDTO);
     return response;
   }
 }
--- trip-planner-user-service/src/main/java/com/felipe/trip_planner_user_service/services/UserService.java
+++ trip-planner-user-service/src/main/java/com/felipe/trip_planner_user_service/services/UserService.java
@@ -1,12 +1,21 @@
 package com.felipe.trip_planner_user_service.services;
 
+import com.felipe.trip_planner_user_service.dtos.UserLoginDTO;
 import com.felipe.trip_planner_user_service.exceptions.UserAlreadyExistsException;
 import com.felipe.trip_planner_user_service.dtos.UserRegisterDTO;
 import com.felipe.trip_planner_user_service.models.User;
 import com.felipe.trip_planner_user_service.repositories.UserRepository;
+import com.felipe.trip_planner_user_service.security.JwtService;
+import com.felipe.trip_planner_user_service.security.UserPrincipal;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Optional;
 
 @Service
@@ -14,10 +23,19 @@ public class UserService {
 
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final AuthenticationManager authenticationManager;
+  private final JwtService jwtService;
 
-  public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+  public UserService(
+    UserRepository userRepository,
+    PasswordEncoder passwordEncoder,
+    AuthenticationManager authenticationManager,
+    JwtService jwtService
+  ) {
     this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
+    this.authenticationManager = authenticationManager;
+    this.jwtService = jwtService;
   }
 
   public User register(UserRegisterDTO userRegisterDTO) {
@@ -33,5 +51,22 @@ public class UserService {
     newUser.setPassword(this.passwordEncoder.encode(userRegisterDTO.password()));
 
     return this.userRepository.save(newUser);
+  }
+
+  public Map<String, Object> login(UserLoginDTO loginDTO) {
+    try {
+      var auth = new UsernamePasswordAuthenticationToken(loginDTO.email(), loginDTO.password());
+      Authentication authentication = this.authenticationManager.authenticate(auth);
+      UserPrincipal userPrincipal = (UserPrincipal) authentication.getPrincipal();
+      String token = this.jwtService.generateToken(userPrincipal);
+
+      Map<String, Object> loginResponse = new HashMap<>(2);
+      loginResponse.put("user", userPrincipal.getUser());
+      loginResponse.put("token", token);
+
+      return loginResponse;
+    } catch(BadCredentialsException e) {
+      throw new BadCredentialsException("Usuário ou senha inválidos", e);
+    }
   }
 }
