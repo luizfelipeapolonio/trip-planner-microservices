d3e42e7a41826d70952033eee7e51e6f022c9694
Luiz Felipe <luizfelipe0527@gmail.com>
2024-08-19 22:55:58 -0300

    feat: add exception handlers

--- trip-planner-user-service/src/main/java/com/felipe/trip_planner_user_service/controllers/ExceptionControllerAdvice.java
+++ trip-planner-user-service/src/main/java/com/felipe/trip_planner_user_service/controllers/ExceptionControllerAdvice.java
@@ -1,12 +1,20 @@
 package com.felipe.trip_planner_user_service.controllers;
 
+import com.auth0.jwt.exceptions.JWTCreationException;
 import com.felipe.trip_planner_user_service.exceptions.UserAlreadyExistsException;
 import com.felipe.trip_planner_user_service.utils.response.CustomResponseBody;
+import com.felipe.trip_planner_user_service.utils.response.CustomValidationErrors;
 import com.felipe.trip_planner_user_service.utils.response.ResponseConditionStatus;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
 
 @RestControllerAdvice
 public class ExceptionControllerAdvice {
@@ -17,6 +25,49 @@ public class ExceptionControllerAdvice {
     CustomResponseBody<Void> response = new CustomResponseBody<>();
     response.setStatus(ResponseConditionStatus.ERROR);
     response.setCode(HttpStatus.CONFLICT);
+    response.setMessage(e.getMessage());
+    response.setData(null);
+    return response;
+  }
+
+  @ExceptionHandler({BadCredentialsException.class, UsernameNotFoundException.class})
+  @ResponseStatus(HttpStatus.UNAUTHORIZED)
+  public CustomResponseBody<Void> handleAuthenticationException(AuthenticationException e) {
+    CustomResponseBody<Void> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.ERROR);
+    response.setCode(HttpStatus.UNAUTHORIZED);
+    response.setMessage(e.getMessage());
+    response.setData(null);
+    return response;
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  @ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
+  public CustomResponseBody<List<CustomValidationErrors>> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+    List<CustomValidationErrors> errors = e.getBindingResult()
+      .getFieldErrors()
+      .stream()
+      .map(fieldError -> new CustomValidationErrors(
+        fieldError.getField(),
+        fieldError.getField().equalsIgnoreCase("password") ? "" : fieldError.getRejectedValue(),
+        fieldError.getDefaultMessage()
+      ))
+      .toList();
+
+    CustomResponseBody<List<CustomValidationErrors>> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.ERROR);
+    response.setCode(HttpStatus.UNPROCESSABLE_ENTITY);
+    response.setMessage("Erros de validação");
+    response.setData(errors);
+    return response;
+  }
+
+  @ExceptionHandler(JWTCreationException.class)
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public CustomResponseBody<Void> handleJWTCreationException(JWTCreationException e) {
+    CustomResponseBody<Void> response = new CustomResponseBody<>();
+    response.setStatus(ResponseConditionStatus.ERROR);
+    response.setCode(HttpStatus.INTERNAL_SERVER_ERROR);
     response.setMessage(e.getMessage());
     response.setData(null);
     return response;
