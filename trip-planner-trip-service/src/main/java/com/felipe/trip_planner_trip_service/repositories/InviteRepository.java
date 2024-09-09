141b584775df952f81e506b6cc2a7566d474d5f2
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-09 18:31:05 -0300

    feat: add method to query by code a valid invite

--- trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/InviteRepository.java
+++ trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/InviteRepository.java
@@ -2,8 +2,14 @@ package com.felipe.trip_planner_trip_service.repositories;
 
 import com.felipe.trip_planner_trip_service.models.Invite;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface InviteRepository extends JpaRepository<Invite, UUID> {
+
+  @Query("SELECT i from Invite i WHERE i.code=:code AND i.isValid=true")
+  Optional<Invite> findByCodeAndIsValidTrue(@Param("code") UUID code);
 }
