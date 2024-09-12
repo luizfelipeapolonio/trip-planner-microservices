846d25860d7b37f85a8f89efd8ac5175d03715d1
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-12 19:19:41 -0300

    feat: add method to find a participant of a trip

--- trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/ParticipantRepository.java
+++ trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/ParticipantRepository.java
@@ -2,8 +2,14 @@ package com.felipe.trip_planner_trip_service.repositories;
 
 import com.felipe.trip_planner_trip_service.models.Participant;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface ParticipantRepository extends JpaRepository<Participant, UUID> {
+
+  @Query("SELECT p FROM Participant p WHERE p.email=:email AND p.trip.id=:tripId")
+  Optional<Participant> findByEmailAndTripId(@Param("email") String email, @Param("tripId") UUID tripId);
 }
