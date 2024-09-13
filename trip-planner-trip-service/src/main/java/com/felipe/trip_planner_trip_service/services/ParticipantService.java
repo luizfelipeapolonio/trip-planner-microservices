8dac8cd350cb0580d982afb7573bd16bc1d5b3ae
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-12 21:20:50 -0300

    feat: delete invite after participant is added

--- trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/services/ParticipantService.java
+++ trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/services/ParticipantService.java
@@ -5,8 +5,11 @@ import com.felipe.trip_planner_trip_service.exceptions.RecordNotFoundException;
 import com.felipe.trip_planner_trip_service.models.Invite;
 import com.felipe.trip_planner_trip_service.models.Participant;
 import com.felipe.trip_planner_trip_service.models.Trip;
+import com.felipe.trip_planner_trip_service.repositories.InviteRepository;
 import com.felipe.trip_planner_trip_service.repositories.ParticipantRepository;
 import com.felipe.trip_planner_trip_service.repositories.TripRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -15,15 +18,19 @@ public class ParticipantService {
   private final ParticipantRepository participantRepository;
   private final InviteService inviteService;
   private final TripRepository tripRepository;
+  private final InviteRepository inviteRepository;
+  private final Logger logger = LoggerFactory.getLogger(ParticipantService.class);
 
   public ParticipantService(
     ParticipantRepository participantRepository,
     InviteService inviteService,
-    TripRepository tripRepository
+    TripRepository tripRepository,
+    InviteRepository inviteRepository
   ) {
     this.participantRepository = participantRepository;
     this.inviteService = inviteService;
     this.tripRepository = tripRepository;
+    this.inviteRepository = inviteRepository;
   }
 
   public Participant addParticipant(AddParticipantDTO addParticipantDTO, String participantEmail, String participantId) {
@@ -37,6 +44,12 @@ public class ParticipantService {
     newParticipant.setEmail(validatedInvite.getUserEmail());
     newParticipant.setTrip(trip);
 
-    return this.participantRepository.save(newParticipant);
+    Participant addedParticipant = this.participantRepository.save(newParticipant);
+    logger.info(
+      "Excluindo invite -> code: {} - email: {} - tripId: {}",
+      validatedInvite.getCode(), validatedInvite.getUserEmail(), validatedInvite.getTrip().getId()
+    );
+    this.inviteRepository.delete(validatedInvite);
+    return addedParticipant;
   }
 }
