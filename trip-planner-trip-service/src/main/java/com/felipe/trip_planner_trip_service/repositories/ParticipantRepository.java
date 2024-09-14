15c9ea56a3463c460abd6e74368fb54cb8e38e00
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-14 20:57:07 -0300

    feat: add method to find all trip participants

--- trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/ParticipantRepository.java
+++ trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/ParticipantRepository.java
@@ -1,6 +1,8 @@
 package com.felipe.trip_planner_trip_service.repositories;
 
 import com.felipe.trip_planner_trip_service.models.Participant;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -12,4 +14,7 @@ public interface ParticipantRepository extends JpaRepository<Participant, UUID> 
 
   @Query("SELECT p FROM Participant p WHERE p.email=:email AND p.trip.id=:tripId")
   Optional<Participant> findByEmailAndTripId(@Param("email") String email, @Param("tripId") UUID tripId);
+
+  @Query("SELECT p FROM Participant p WHERE p.trip.id=:tripId")
+  Page<Participant> findAllByTripId(@Param("tripId") UUID tripId, Pageable pageable);
 }
