e2c8bc305d7c656aefba6b0642615aee0df8aefd
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-20 20:20:46 -0300

    feat: add method to find all trips that authenticated user is a participant

--- trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/TripRepository.java
+++ trip-planner-trip-service/src/main/java/com/felipe/trip_planner_trip_service/repositories/TripRepository.java
@@ -4,6 +4,8 @@ import com.felipe.trip_planner_trip_service.models.Trip;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.UUID;
@@ -11,4 +13,7 @@ import java.util.UUID;
 public interface TripRepository extends JpaRepository<Trip, UUID> {
   Page<Trip> findAllByOwnerEmail(String ownerEmail, Pageable pageable);
   List<Trip> findAllByOwnerEmail(String ownerEmail);
+
+  @Query("SELECT t FROM Trip t JOIN t.participants p WHERE p.email=:participantEmail")
+  Page<Trip> findAllByParticipantEmail(@Param("participantEmail") String participantEmail, Pageable pageable);
 }
