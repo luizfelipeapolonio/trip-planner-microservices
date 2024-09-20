2f81846820f819d10f0c50dfd1d062ab2762a5db
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-20 20:24:13 -0300

    improvement: add tests environment configuration

--- trip-planner-trip-service/src/test/java/com/felipe/trip_planner_trip_service/TripPlannerTripServiceApplicationTests.java
+++ trip-planner-trip-service/src/test/java/com/felipe/trip_planner_trip_service/TripPlannerTripServiceApplicationTests.java
@@ -1,9 +1,14 @@
 package com.felipe.trip_planner_trip_service;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.kafka.KafkaAutoConfiguration;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles(value = "test")
+@EnableAutoConfiguration(exclude = {KafkaAutoConfiguration.class})
 class TripPlannerTripServiceApplicationTests {
 
 	@Test
