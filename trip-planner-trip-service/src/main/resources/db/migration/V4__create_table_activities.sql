4eb0b018e2ddd59264fa807fa7378da32c84c546
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-23 23:06:17 -0300

    feat: add migration to create activities table

---
+++ trip-planner-trip-service/src/main/resources/db/migration/V4__create_table_activities.sql
@@ -0,0 +1,9 @@
+CREATE TABLE activities (
+    id          UUID         NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    description VARCHAR(255) NOT NULL,
+    trip_id     UUID         NOT NULL,
+    owner_email VARCHAR(255) NOT NULL,
+    created_at  TIMESTAMP    NOT NULL DEFAULT  CURRENT_TIMESTAMP,
+
+    CONSTRAINT fk_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
