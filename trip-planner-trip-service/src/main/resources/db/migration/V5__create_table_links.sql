91c57288d0bf52abd2ff76e06b52e3de4e86f7f9
Luiz Felipe <luizfelipe0527@gmail.com>
2024-09-26 18:58:24 -0300

    feat: add create links table migration

---
+++ trip-planner-trip-service/src/main/resources/db/migration/V5__create_table_links.sql
@@ -0,0 +1,11 @@
+CREATE TABLE links (
+    id          UUID         NOT NULL PRIMARY KEY DEFAULT gen_random_uuid(),
+    title       VARCHAR(255) NOT NULL,
+    link        VARCHAR(255) NOT NULL,
+    owner_email VARCHAR(255) NOT NULL,
+    trip_id     UUID         NOT NULL,
+    created_at  TIMESTAMP    NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at  TIMESTAMP    NOT NULL,
+
+    CONSTRAINT fk_trip FOREIGN KEY (trip_id) REFERENCES trips(id) ON UPDATE CASCADE ON DELETE CASCADE
+);
